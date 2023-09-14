9f2c32fd31b0146049398401d8b7de1fe806695f
sAndrin3 <sandrinemalanga2001@gmail.com>
2023-09-14 17:40:27 +0300

    Forms

---
+++ BlazorForms/Models/CarDto.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorForms.Models
+{
+    public class CarDto
+    {
+
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        public string Description { get; set; }=string.Empty;
+        [Required]
+        public string Color { get; set; } = string.Empty;
+
+        [Required]
+        [Range(0,10000)]
+        public int Mileage { get; set; }
+
+        public List<Make> makes { get; set; }= new List<Make>();
+
+
+    }
+
+    public class Make
+    {
+        public int Id { get; set; }
+        [Required]
+        public string ModelName { get; set; }
+    }
+}
