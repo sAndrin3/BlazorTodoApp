b445a172297aba3920bbd808cbe1aa1f52cbb99a
sAndrin3 <sandrinemalanga2001@gmail.com>
2023-09-14 22:48:33 +0300

    Validation

--- MyBlazorApp/Models/TodoDto.cs
+++ MyBlazorApp/Models/TodoDto.cs
@@ -1,11 +1,23 @@
-namespace MyBlazorApp.Models{
-    public class TodoDto{
-        public Guid Id {get; set; }
-        public string Title {get; set; } = string.Empty;
-        public string Description {get; set;} = string.Empty;
-        public string Priority {get; set; } = string.Empty;
-        public string Duration {get; set; } = string.Empty;
-       public DateTime CreatedAt { get; set; } = DateTime.Now;
-     
+using System.ComponentModel.DataAnnotations;
+
+namespace MyBlazorApp.Models
+{
+    public class TodoDto
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Title is required.")]
+        public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Description is required.")]
+        public string Description { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Priority is required.")]
+        public string Priority { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Duration is required.")]
+        public string Duration { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
 }
